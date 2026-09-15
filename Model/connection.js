506c11453163a0d3fde970a5591e6661e3179e24
devilcry3 <78716116+devilcry3@users.noreply.github.com>
2026-09-15 14:17:51 -0600

    finally fixed once and for all (hopefully)

--- Model/connection.js
+++ Model/connection.js
@@ -18,7 +18,7 @@ async function setDatabaseConnction() {
     //Singleton DB connection
     if (null === connection) {
         connection = await mysql.createConnection({
-            host: '://amazonaws.com',
+            host: 'tractor-database.cnmi82ki8tut.us-east-2.rds.amazonaws.com',
             port: 3306,
             user: 'admin',
             password: 'traktortraktor',
